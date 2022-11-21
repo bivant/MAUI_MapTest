1dca7ad24fbab44021ecbe8f3a8f12b9acdf9d12
Ivan <6350992+bivant@users.noreply.github.com>
2022-11-21 22:20:56 +0300

    Code to show the pin touch fall through to map (on iOS, Android works as expected)

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,33 +20,9 @@ public partial class MainPage : ContentPage
         });
     }
 
-    void OnBuildClicked(object sender, EventArgs e)
+    async void OnMapClicked(object sender, MapClickedEventArgs e)
     {
-        var pinLocation = new Location(20.7557, -155.9880);
-        map.Pins.Add(new Pin
-        {
-            Label = "MAUI Test Pin",
-            Location = pinLocation
-        });
-
-        var route = new Polyline()
-        {
-            StrokeWidth = 8,
-            StrokeColor = Color.Parse("#1BA1E2")
-        };
-        route.Geopath.Add(pinLocation);
-        route.Geopath.Add(new Location(20.750561, -155.986836));
-        route.Geopath.Add(new Location(20.747872, -155.986332));
-        route.Geopath.Add(new Location(20.743709, -155.985872));
-        route.Geopath.Add(new Location(20.741195, -155.985788));
-        route.Geopath.Add(new Location(20.738743, -155.985399));
-        map.MapElements.Add(route);
-    }
-
-    void OnClearClicked(object sender, EventArgs e)
-    {
-        map.Pins.Clear();
-        map.MapElements.Clear();
+        await DisplayAlert("Map", $"Area {e.Location.Latitude}, {e.Location.Longitude} clicked.", "Ok");
     }
 }
 
